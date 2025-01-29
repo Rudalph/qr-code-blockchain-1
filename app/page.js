1c0056f1778fc8e2cdf925884424415c55b43049
Rudalph <gonsalvesrudalph@gmail.com>
2025-01-29 23:42:31 +0530

    backend connection updated

--- app/page.js
+++ app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
     console.log("Form Data:", data);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/qrcode', {
+      const response = await fetch('https://qr-generator-0tqz.onrender.com/qrcode', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
